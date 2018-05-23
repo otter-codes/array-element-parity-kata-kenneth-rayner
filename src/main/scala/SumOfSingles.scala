c6b27f32d066df1be455fa430eda8b56da92dac1
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-23 15:43:31 +0100

    refactor code

--- src/main/scala/SumOfSingles.scala
+++ src/main/scala/SumOfSingles.scala
@@ -1,11 +1,6 @@
 object SumOfSingles {
 
-  def total (input:List[Int]):Int = {
-    input.sum
-  }
-
-  def singleEntries(input:List[Int]):Int= {
-
+  def sumNonRepeatedNos(input:List[Int]):Int= {
     val inputDistinct =input.distinct
     val repeatedNumbers = input diff inputDistinct
     val nonRepeatedNumbers =inputDistinct diff repeatedNumbers
