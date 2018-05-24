f28726241974d6944c83c87fdfb945f7ca7f3015
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-05-24 08:45:26 +0100

    add alternative using map function

--- src/main/scala/SumOfSingles.scala
+++ src/main/scala/SumOfSingles.scala
@@ -3,7 +3,9 @@ object SumOfSingles {
   def sumNonRepeatedNos(input:List[Int]):Int= {
 
     val repeatedNumbers = input diff input.distinct
-    val nonRepeatedNumbers =input.distinct diff repeatedNumbers
-    nonRepeatedNumbers.sum
+   //val nonRepeatedNumbers =input.distinct diff repeatedNumbers
+   // nonRepeatedNumbers.sum
+   // alternative :
+     input.filterNot(x => repeatedNumbers.contains(x)).sum
   }
 }
